1878ba267e6314ffe81a183411562e9137090f83
DylanLev <dylan.levy5@yahoo.com>
2021-12-30 20:17:16 +0200

    Summary
    destination page beer sheva

--- destination_page.php
+++ destination_page.php
@@ -78,7 +78,8 @@
         <div class="item">
             <img src="img/jerusalem.jpg">
             <h3><b>Beer Sheva</b></h3>
-						<p>Be'er Sheva is located in the northern Negev, known as the "capital of the Negev and the south" due to being the central and largest city in the region. This city shows the progress of the country</p>
+						<p>FacebookWhatsAppTwitterPinterestCopy Link
+Beer Sheva is the capital of the Negev region of Israel, a desert city that serves as the gateway to this region. There are a number of interesting things to do in Beer Sheva, an ancient city dating from Biblical times, including the old Turkish town, which is the only planned Ottoman city in the region, the Turkish railway station, and the Bedouin market, as well as Tel Beer Sheba, an UNESCO World Heritage Site.</p>
             <button id="place_1" onclick="place_1()">View More</button>
 
         </div>
