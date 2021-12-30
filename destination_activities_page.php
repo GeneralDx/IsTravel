517675aa4706e8c5341e4d97c35258ea4f227ec1
GeneralDx <39958539+GeneralDx@users.noreply.github.com>
2021-12-30 21:10:03 +0200

    Summary
    77th commit (destination_page)

--- destination_activities_page.php
+++ destination_activities_page.php
@@ -78,14 +78,13 @@
         <div class="item">
             <img src="img/jerusalem.jpg">
             <h3><b>Beer Sheva</b></h3>
-            <p>
-Beer Sheva is the capital of the Negev region of Israel, a desert city that serves as the gateway to this region. There are a number of interesting things to do in Beer Sheva, an ancient city dating from Biblical times, including the old Turkish town, which is the only planned Ottoman city in the region, the Turkish railway station, and the Bedouin market, as well as Tel Beer Sheba which is the archaeological remnants of the ancient city and UNESCO World Heritage Site.</p>
+						<p>Be'er Sheva is located in the northern Negev, known as the "capital of the Negev and the south" due to being the central and largest city in the region. This city shows the progress of the country</p>
             <button id="city_1" onclick="city_1()">View More</button>
 
         </div>
         <div class="item">
         <img src="img/haifa.jpg">
-            <h3><b>Haifa</b></h3>
+            <h3><b>Dead Sea</b></h3>
             <p>Modern Haifa is a bustling port town, but unlike many industry-focused cities, its landscape of steep cliffs rolling down to the shore gives it a beautiful setting.
             For travelers who want a base with both good access to the sights of the north and plentiful city buzz, with a thriving café and dining scene, this is one of the best places to visit.
             Haifa's hillside location is enhanced by the Baha'i Gardens, which are the central city's dominating feature.</p>
@@ -93,36 +92,36 @@ Beer Sheva is the capital of the Negev region of Israel, a desert city that serv
         </div>
         <div class="item">
         <img src="img/telaviv.jpg">
-            <h3><b>Tel Aviv</b></h3>
+            <h3><b>Eilat</b></h3>
             <p>With its hot artistic vibe, miles of picturesque city-side beaches, fascinating neighborhoods and a thriving nightlife, Tel Aviv seems to have it all. Yet Israel’s second largest—and no doubt hippest—city remains under-appreciated by most travelers. Thankfully, a series of hotel openings, city-wide renovations and a renewed appreciation for the city’s historic offerings has finally started to garner this glam metropolis the attention it so rightly deserves.</p>
             <button id="city_3" onclick="city_3()">View More</button>
 
         </div>
         <div class="item">
         <img src="img/eilat.jpg">
-            <h3><b>Eilat</b></h3>
+            <h3><b>Haifa</b></h3>
             <p>Eilat is Israel's southernmost city and the country's only outpost on the shores of the Red Sea, which it shares with three other nations: Jordan and Egypt, which both have a land border with the city. As the country's vacation city par excellence, many Israelis insist on visiting Eilat at least once a year or, at the very least, going through it en route to neighboring destinations, when they incorporate an overnight there with a vacation in Sinai or a trip to Jordan.</p>
             <button id="city_4" onclick="city_4()">View More</button>
         </div>
 
         <div class="item">
         <img src="img/eingedi.jpg">
-            <h3><b>Ein Gedi</b></h3>
+            <h3><b>Jerusalem</b></h3>
             <p>Ein Gedi, just aside the Dead Sea in the Judean Desert, is one of Israel’s premier hiking spots, featuring spectacular beauty, varied landscapes, and botanical gardens. There’s no doubt that Ein Gedi Nature Reserve is one of the most beautiful places in Israel. While it is located close to Jerusalem, it feels worlds away, Ein Gedi is, of course, one of the most popular escape spots for locals and tourists who take advantage of the reserve, botanical gardens, and the Dead Sea.</p>
             <button id="city_5" onclick="city_5()">View More</button>
         </div>
         <div class="item">
             <img src="img/tiberias.jpg">
-            <h3><b>Tiberias</b></h3>
+            <h3><b>Mitspe Ramon</b></h3>
             <p>Sitting on the edge of the Sea of Galilee, Tiberias is the perfect base to explore this region. It is a famous historical and tourist town. The ancient city is located in the southern part of today's agglomeration.
-Flowing with natural hot springs, brimming with Christian and Jewish holy sites and hugging the shores of the Sea of Galilee, Tiberias is a top tourist stopover for visitors to Israel. The city boasts many historical and religious sites.</p>
+Flowing with natural hot springs, brimming with Christian and Jewish holy sites and hugging the shores of the Sea of Galilee, Tiberias is a top tourist stopover for visitors to Israel. The city boasts many historical and religious sites as well as water recreation options.</p>
             <button id="city_6" onclick="city_6()">View More</button>
 
         </div>
 
         <div class="item">
         <img src="img/jaffa.jpg">
-            <h3><b>Jaffa</b></h3>
+            <h3><b>Netanya</b></h3>
             <p>Jaffa (also known as Yafo) is the ancient port city out of which Tel Aviv has now grown. In recent years, like much of South Tel Aviv, this area has been regenerated. The old narrow streets and courtyards are now another highly desirable part of Tel Aviv’s urban tapestry.
 Jaffa’s flea market is a well-known attraction of the area. Here, the vendors sell a diverse range of interesting and unique products from Turkish carpets to original art. Meanwhile, the narrow passageways and ancient buildings in the Old City are worlds away from modern Tel Aviv.</p>
             <button id="city_7" onclick="city_7()">View More</button>
@@ -130,14 +129,14 @@ Jaffa’s flea market is a well-known attraction of the area. Here, the vendors 
         </div>
         <div class="item">
         <img src="img/ashdod.jpg">
-            <h3><b>Ashdod</b></h3>
+            <h3><b>Tel Aviv</b></h3>
             <p>Ashdod is the sixth-largest city and the largest port in Israel accounting for 60% of the country's imported goods. Ashdod is located in the Southern District of the country, on the Mediterranean coast where it is situated between Tel Aviv to the north and Ashkelon. The city is also an important regional industrial center.Modern Ashdod covers the territory of two ancient twin towns, one inland and one on the coast, which were for most of their history two separate entities, connected though by close ties with each other.</p>
             <button id="city_8" onclick="city_8()">View More</button>
 
         </div>
         <div class="item">
         <img src="img/nahariya.jpg">
-            <h3><b>Nahariya</b></h3>
+            <h3><b>Tiberias</b></h3>
             <p>Nahariya is Israel’s northernmost coastal city, a last testament to the seaside settlements that reach up along the sunny Mediterranean shoreline. Being the end of the train-line and just a few minutes from Rosh HaNikra, with its famous white sandstone grottos, tourists often find themselves Nahariya, a vibrant, little city. With a lively nucleus, comprised of a wide boulevard, divided by a river and lined with shops, restaurants, bars and hotels, and the seafront bars and restaurants, Nahariya provides sun-lovers with an idyllic vacation spot.</p>
             <button id="city_9" onclick="city_9()">View More</button>
 				</div>
